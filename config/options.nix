8bf92389fc7be6795084425b1f67a9fdee3c28ad
awheeler45 <awheeler45@gmail.com>
2025-03-13 14:09:20 -0500

    Update options.nix

--- config/options.nix
+++ config/options.nix
@@ -3,7 +3,7 @@
     clipboard.providers.wl-copy.enable = true;
     # Hook this in with a fuyu-no-kosei service? Stylix does have a nixvim option.
     colorschemes.everforest.enable = true;
-    colorschemes.everforest.settings.transparent_background = 1;
+    colorschemes.everforest.settings.transparent_background = 2;
     editorconfig.enable = true;
     opts = {
       listchars = {
